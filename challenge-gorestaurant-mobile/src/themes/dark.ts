1234e87c2fa06f3331dd675c0f65d4856b57ce46
Stefano Saffran <stefanosaffran@hotmail.com>
2020-06-25 18:41:29 +0200

    feat: add dark theme

---
+++ challenge-gorestaurant-mobile/src/themes/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    background: '#333',
+    text: '#b3b3b3',
+    title: '#f5f5f5',
+    titleAlternative: '#fff',
+
+    cardBackground: '#222',
+    selected: '#140000',
+    placeholder: '#999',
+  },
+};
